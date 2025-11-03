dcf3b25b4d537f130f2a28a879a2d58ca3ca2c9a
RochaGR <gutop.rocha@gmail.com>
2025-11-03 20:39:56 -0300

    Fix: telas e add data.sql para add admin

--- barbearia/src/main/java/com/barbearia/agendamento/BarbeariaAgendamentoApplication.java
+++ barbearia/src/main/java/com/barbearia/agendamento/BarbeariaAgendamentoApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BarbeariaAgendamentoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BarbeariaAgendamentoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BarbeariaAgendamentoApplication.class, args);
+    }
 }
