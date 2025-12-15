090a2b526671269ee7384e48198c94c44ad89cd9
RochaGR <gutop.rocha@gmail.com>
2025-12-14 22:49:28 -0300

    insano

--- barbearia/src/main/resources/data.sql
+++ barbearia/src/main/resources/data.sql
@@ -1,7 +1,7 @@
--- Inserir administrador padrão
--- Email: admin.souza@gmail.com
--- Senha: Senha@123 (criptografada com BCrypt)
 INSERT INTO administradores (nome, email, senha, telefone)
 SELECT 'Admin', 'admin.souza@gmail.com', '$2a$10$2mEpSiktdueKBjUl7Y7A.OkrBw.f/DKl4.OWxf796a5IdH8EwXkQq', NULL
 WHERE NOT EXISTS (SELECT 1 FROM administradores WHERE email = 'admin.souza@gmail.com');
+
+
+
 
